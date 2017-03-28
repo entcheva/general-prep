5c342971b02e4e7b8160ab683e463cf76b80b9ac
entcheva <christina.entcheva@gmail.com>
2017-03-27 23:57:36 -0400

    AddUsernameToUsers

--- intermediate_rails/shouter/db/schema.rb
+++ intermediate_rails/shouter/db/schema.rb
@@ -10,14 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170328032836) do
+ActiveRecord::Schema.define(version: 20170328035136) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "username"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["username"], name: "index_users_on_username"
   end
 
 end
